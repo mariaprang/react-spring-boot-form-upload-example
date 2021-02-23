a392fd317bf2dd1f92f8c7d1a3ef9601652f46ff
mariaprang <marie.prangishvili@gmail.com>
2021-02-23 17:15:17 +0400

    Initial commit

---
+++ src/main/java/formuploadfullstack/demo/FormUploadFullStackApplication.java
@@ -0,0 +1,13 @@
+package formuploadfullstack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormUploadFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormUploadFullStackApplication.class, args);
+    }
+
+}
